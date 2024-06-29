acbf74eba0c2df0992d595d9487f15a7032d81d4
amir_abbas <abas.aslmi0007@gmail.com>
2024-06-29 13:28:17 +0330

    push

--- lib/src/countdown_base.dart
+++ lib/src/countdown_base.dart
@@ -7,20 +7,22 @@ import "dart:async";
 
 class CountDown {
   /// reference point for start and resume
-  DateTime _begin;
-  Timer _timer;
-  Duration _duration;
-  Duration remainingTime;
+  DateTime? _begin;
+  Timer? _timer;
+  Duration? _duration;
+  Duration? remainingTime;
   bool isPaused = false;
-  StreamController<Duration> _controller;
-  Duration _refresh;
+  StreamController<Duration>? _controller;
+  Duration? _refresh;
 
   /// provide a way to send less data to the client but keep the data of the timer up to date
-  int _everyTick, counter = 0;
+  int _everyTick = 0;
+  int counter = 0;
 
   /// once you instantiate the CountDown you need to register to receive information
   CountDown(Duration duration,
-      {Duration refresh: const Duration(milliseconds: 10), int everyTick: 1}) {
+      {Duration refresh = const Duration(milliseconds: 10),
+      int everyTick = 1}) {
     _refresh = refresh;
     _everyTick = everyTick;
 
@@ -32,20 +34,20 @@ class CountDown {
         onCancel: _onCancel);
   }
 
-  Stream<Duration> get stream => _controller.stream;
+  Stream<Duration> get stream => _controller!.stream;
 
   /// _onListen
   /// invoke when the first subscriber has subscribe and not before to avoid leak of memory
   _onListen() {
     // reference point
     _begin = DateTime.now();
-    _timer = Timer.periodic(_refresh, _tick);
+    _timer = Timer.periodic(_refresh!, _tick);
   }
 
   /// the remaining time is set at '_refresh' ms accurate
   _onPause() {
     isPaused = true;
-    _timer.cancel();
+    _timer?.cancel();
     _timer = null;
   }
 
@@ -57,13 +59,13 @@ class CountDown {
     isPaused = false;
 
     //  lance le timer
-    _timer = Timer.periodic(_refresh, _tick);
+    _timer = Timer.periodic(_refresh!, _tick);
   }
 
   _onCancel() {
     // on pause we already cancel the _timer
     if (!isPaused) {
-      _timer.cancel();
+      _timer?.cancel();
       _timer = null;
     }
     // _controller.close(); // close automatically the "pipe" when the sub close it by sub.cancel()
@@ -71,17 +73,17 @@ class CountDown {
 
   void _tick(Timer timer) {
     counter++;
-    Duration alreadyConsumed = DateTime.now().difference(_begin);
-    this.remainingTime = this._duration - alreadyConsumed;
-    if (this.remainingTime.isNegative) {
+    Duration alreadyConsumed = DateTime.now().difference(_begin!);
+    this.remainingTime = this._duration! - alreadyConsumed;
+    if (this.remainingTime!.isNegative) {
       timer.cancel();
-      timer = null;
+      timer = Timer(Duration(), () {});
       // tell the onDone's subscriber that it's finish
-      _controller.close();
+      _controller?.close();
     } else {
       // here we can control the frequency of sending data
       if (counter % _everyTick == 0) {
-        _controller.add(this.remainingTime);
+        _controller?.add(this.remainingTime!);
         counter = 0;
       }
     }
